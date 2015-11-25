77c7f82076239aa7cce14ad67a179605f04776d6
David Mamujee <dmamujee@yahoo.ca>
2015-11-25 14:16:53 -0500

    Create game.h

---
+++ game.h
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Piece;
+class Cell;
+
+class Game{
+	Cell** theBoard;
+	Piece* playerWhite[16];
+	Piece* playerBlack[16];
+	GameNotifications* notifications;
+	int p1Score;
+	int p2Score;
+	void clearGame();
+
+
+public:
+	void notify();// What will the parameters for this be?
+	bool isWon();
+	bool isCheck();
+	bool isValidMove(int x1, int y1, int x2, int y2);
+	bool setup(); // Do you think this is neccesary?
+	bool makeMove(int x1, int y1, int x2, int y2);
+
+
+};
+
+#endif
