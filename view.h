9a797a3621fa18c1e8909a81791d589ca62e1122
David Mamujee <dmamujee@yahoo.ca>
2015-11-25 14:25:50 -0500

    Create view.h

---
+++ view.h
@@ -0,0 +1,22 @@
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include <iostream>
+#include <sstream>
+
+class View {
+
+  public:
+    View(int);
+    
+ /*
+  * The Controller calls notify to update the (row,column) location to be ch
+  */
+ virtual void notify(int row, int column, char ch) = 0;
+
+ virtual void print(std::ostream &out) const = 0;
+    
+ virtual ~View() = 0;
+};
+
+#endif
