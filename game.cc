df9ca5db44c531bf2c4a96970ca3288e110a7c0d
David Mamujee <dmamujee@yahoo.ca>
2015-11-26 14:15:06 -0500

    Update game.cc

--- game.cc
+++ game.cc
@@ -15,29 +15,29 @@ void Game::clearGame(){
 
 bool Game::isPossibleMove(int startX, int startY, int endX, int endY){
 	//Checks if the Move is valid based on the isMoveValid() function from the Piece class
-	if (theBoard[startX][startY]->isMoveValid(startX,startY,endX,endY) == false) return false;
+	if (theBoard[startX][startY]->getPiece()->isMoveValid(startX,startY,endX,endY) == false) return false;
 
 	//Since Knights only can move in certain ways, if it passes the Piece isMoveValid(), it is valid
-	if (theBoard[startX][startY]->getName == 'n' || theBoard[startX][startY]->getName == 'N') return true;
+	if (theBoard[startX][startY]->getPiece()->getName() == 'n' || theBoard[startX][startY]->getPiece()->getName()== 'N') return true;
 
 	// Checks that the pawn's move is valid
-	if (theBoard[startX][startY]->getName == 'p' || theBoard[startX][startY]->getName == 'P'){
+	if (theBoard[startX][startY]->getPiece()->getName() == 'p' || theBoard[startX][startY]->getPiece()->getName()me == 'P'){
 		//Case if pawn is moving forward, not capturing
 		if (endX == endY){
 			//Checks that the space it is moving to is empty
-			if (theBoard[endX][endY]->piece == NULL) return true;
+			if (theBoard[endX][endY]->getPiece() == NULL) return true;
 			else return false;
 		}
 		//Checks that the space it is moving to is occupied by opposing piece
 		else{
-			if (theBoard[endX][endY]->piece == NULL) return false;
+			if (theBoard[endX][endY]->getPiece() == NULL) return false;
 			else{
 				//If the piece is White
-				if(theBoard[startX][startY]->getName >= 'A'){
+				if(theBoard[startX][startY]->getName() >= 'A'){
 					// If it finds the piece that is in the location the pawn is moving to is an enemy piece
 					// it returns true. otherwise the returns false
 					for (int i = 0; i < 16; i++){
-						if (playerBlack[i] == theBoard[endX][endY]->getPiece){
+						if (playerBlack[i] == theBoard[endX][endY]->getPiece()){
 							return true;
 						}
 					}
@@ -47,7 +47,7 @@ bool Game::isPossibleMove(int startX, int startY, int endX, int endY){
 				//if the piece is Black
 				else{
 					for (int i = 0; i < 16; i++){
-						if (playerWhite[i] == theBoard[endX][endY]->getPiece){
+						if (playerWhite[i] == theBoard[endX][endY]->getPiece()){
 							return true;
 						}
 					}
@@ -58,8 +58,34 @@ bool Game::isPossibleMove(int startX, int startY, int endX, int endY){
 	}
 
 	//Checks that bishop move is valid
-	if (theBoard[startX][startY]->getName == 'b' || theBoard[startX][startY]->getName == 'B'){
-		
+	if (theBoard[startX][startY]->getPiece()->getName() == 'b' || theBoard[startX][startY]->getPiece()->getName() == 'B'){
+		// Bishop is moving the the right
+		if (endX > startX){
+			for (int i = 1; i < endX - startX; i++){
+				// Bishop is moving diagonaly up to the right
+				if (endY > startY){
+					if (theBoard[startX + i][startY + i]->getPiece() != NULL) return false;
+				}
+				// Bishop is moving down to the right
+				else{
+					if (theBoard[startX + i][startY - i]->getPiece() != NULL) return false;	
+				}
+			}
+		}
+
+		// Bishop is moving to the left
+		else{
+		for (int i = 1; i < startX - endX; i++){
+				// Bishop is moving diagonaly up to the right
+				if (endY > startY){
+					if (theBoard[startX - i][startY + i]->getPiece() != NULL) return false;
+				}
+				// Bishop is moving down to the right
+				else{
+					if (theBoard[startX - i][startY - i]->getPiece() != NULL) return false;	
+				}
+			}
+		}
 	}
 
 
