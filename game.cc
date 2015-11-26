f62653ddea84ae0c0f88c237819225216b65816d
David Mamujee <dmamujee@yahoo.ca>
2015-11-25 23:59:48 -0500

    isCheck() Function
    The implementation for isCheck is not complete, as there currently is no way to access the coordinates of a Piece from the Piece

--- game.cc
+++ game.cc
@@ -12,6 +12,43 @@ void clearGame(){
 
 }
 
-bool isCheck(){
-	
+bool isCheck(int player){
+	if (player == WHITE){
+		//Finds the Location the the King the the array of pieces and create pointer to it
+		for (int i = 0; i < 16; i++){
+			if (playerWhite[i]!= NULL && playerWhite[i]->getName() == 'K'){
+				Piece *king = playerWhite[i];
+				break;
+			}
+		}
+
+		for (int i = 0; i < 16; i++){
+			if (playerWhite[i] != king && playerWhite[i] != NULL){
+				if (isPossibleMove(playerWhite[i]->x,playerWhite[i]->y,king->x,king->y){
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+	else if (player == BLACK){
+		for (int i = 0; i < 16; i++){
+			if (playerBlack[i]!= NULL && playerBlack[i]->getName() == 'k'){
+				Piece *king = playerBlack[i];
+				break;
+			}
+		}
+
+		for (int i = 0; i < 16; i++){
+			if (playerBlack[i] != king && playerBlack[i] != NULL){
+				if (isPossibleMove(playerBlack[i]->x,playerBlack[i]->y,king->x,king->y){
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+	else return false;
 }
