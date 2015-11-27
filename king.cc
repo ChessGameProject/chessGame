49104edd7b0afb3d29d407cdba6f3e69f090c9e4
Jonathan Smith <jsmith@interset.com>
2015-11-27 00:20:54 -0500

    Prepare some of the king's moves

--- king.cc
+++ king.cc
@@ -9,8 +9,28 @@ King::King(int player) : Piece(player, 'k') {
 
 // Checks if a given move is valid
 bool King::isMoveValid(int endX, int endY) const {
-	if ( !hasMoved && ( x - endX == 2 ) ) { // Is moving two to the left	
-		// Check Castling
+	if ( std::abs(x - endX) == 2 ) { // Is moving two
+		// Commence Castling Check
+
+		// Check that king hasn't moved
+		if (hasMoved) return false;
+
+		// Determine direction
+		int dir = 1
+		if (endX - x < 0) dir = -1;
+
+		// TODO: Check the rook in that direction has not moved
+
+		// Check pieces between king and rook
+		for (int i = x; ((x > 0) && (x < 8)); x += dir) {
+			if game.isOccupied(i, y) return false;
+		}
+
+		// TODO: Check that king isn't currently in check
+
+		// TODO: Check that squares the King WOULD pass through would not
+		//    place the King in check
+
 		return true;
 	} else if ( ( std::abs(endX-x) <= 1 ) && ( std::abs(endY - y) <= 1 ) ) {
 		// Otherwise only move one square in any direction
