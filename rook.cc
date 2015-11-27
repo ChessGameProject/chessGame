759a165014c00a44db856de5aba8b0dade75fd8f
Jonathan Smith <jsmith@interset.com>
2015-11-26 23:16:48 -0500

    Add checking pieces in between to Bishop and Rook

--- rook.cc
+++ rook.cc
@@ -6,9 +6,19 @@ Rook::Rook(int player) : Piece(player, 'r') {}
 
 // Checks to see if a given move is valid
 bool Rook::isMoveValid(int endX, int endY) const {
-	if ( (endX - x) == 0 ||
-			 (endY - y) == 0 ) {
-		// Moves must be stright
+	if ( (endX - x) == 0 ) {
+		// Straight up and down
+		
+		// Determine direction 
+		int yDir = 1;
+		if (endY - y < 0) yDir = -1;
+
+		for ( int i = y; i != endY; i += yDir ) {
+			if ( game.isOccupied(x, i) ) return false;
+		}
+
+	} else if ( (endY - y) == 0 ) {
+		// Stright right or left
 		return true;
 	}
 
