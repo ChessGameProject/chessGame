dbca81cda8f5c606e86d3fb9547e861f642bb58e
Jonathan Smith <jsmith@interset.com>
2015-12-01 01:32:04 -0500

    Check pawn square occupation for forward movement

--- pawn.cc
+++ pawn.cc
@@ -25,7 +25,8 @@ bool Pawn::isMoveValid(int endX, int endY) const {
 
 		return true;
 	} else if ( (endX - x) == 0 &&
-			 			( (endY - y) / direction ) == 1 ) {
+			 			  ( ( (endY - y) / direction ) == 1 ) &&
+			 			  !(game->isOccupied(endX, endY)) ) {
 		// Move one square away from starting end of the board
 		return true;
 	} else if ( (endX - x) == 1 &&
